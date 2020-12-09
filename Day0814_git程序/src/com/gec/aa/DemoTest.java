f06ffd95e2700862431520c82ee6fbfd9e58465d
TaTaGEsound <956486387@qq.com>
2020-12-09 21:08:13 +0800

    1209

--- Day0814_git程序/src/com/gec/aa/DemoTest.java
+++ Day0814_git程序/src/com/gec/aa/DemoTest.java
@@ -1,5 +1,17 @@
 package com.gec.aa;
 
+/**
+ * 
+  * 功能描述:
+  *          第一步:关联本地仓库（项目会有问号）s
+  *          若没有关联本地仓库，则Team下会有share Porject选项
+  * 
+ *		第二步:上传到远程仓库(Commit and Push)
+ *		关联本地后，会有Commit选项
+ *
+ * @date 2020年8月14日
+ */
+
 public interface DemoTest {
 	
 	void addUser();
